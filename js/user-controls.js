5f3e53012fd75ed3dac3f05e8daef7223bb4635c
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-07 20:07:40 +0000

    added user controls

--- js/user-controls.js
+++ js/user-controls.js
@@ -1,4 +1,22 @@
 
 function respondToUser() {
+    if (keyDown("UP")) {
+        player.velocityY = player.velocityY - 0.5;
 
+    }
+    // change the y velocity when the user clicks "down"
+    if (keyDown("DOWN")) {
+        player.velocityY = player.velocityY + 0.5;
+
+    }
+
+    // decrease the x velocity when user clicks "left"
+    if (keyDown("LEFT")) {
+        player.velocityX = player.velocityX - 0.5;
+    }
+
+    // increase the x velocity when the user clicks "right"
+    if (keyDown("RIGHT")) {
+        player.velocityX = player.velocityX + 0.5;
+    }
 }

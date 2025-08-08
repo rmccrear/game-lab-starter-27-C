2711e184df473dceee2f5cada35fecbd37a8b91a
Lashon81 <lashon_riley81@yahoo.com>
2025-08-07 20:49:55 -0400

    changes

--- js/sprite-setup.js
+++ js/sprite-setup.js
@@ -1,23 +1,25 @@
-
-function setupSprites() {
-  // Example Code follows.
-  // Please delete and replace with your own code.
-  player = createSprite(200, 200, 50, 50);
-  player.addAnimation('fly', alienAnimation);
-  player.height = 50;
-  player.width = 50;
-
- //tar = createSprite(100, 100, 50, 50);
- //tar.shapeColor = "aqua";
-  // star.addAnimation('twinkle', starAnimation);
-  // star.velocityX = -1;
-  // star.velocityY = -2;
-  // star.height = 30;
-  // star.width = 30;
-
-  platform1 = createSprite(randomNumber(50,300),0, 20, 75);
-  platform1.addAnimation('platform.png');
-
-  platform2 = createSprite(randomNumber(50,300),0, 20, 75);
-  platform2.addAnimation('platform.png');
+function spritesetup() {  
+// create player, target, and obstacles
+var player = createSprite(200, 100);
+player.setAnimation("kid_34_excited_1");
+player.scale = 0.2;
+var coins = createSprite(364, 19);
+coins.setAnimation("coin");
+var card = createSprite(6, 140);
+card.setAnimation("cardClubsQ_1");
+card.scale = 0.3;
+var card2 = createSprite(297, 6);
+card2.setAnimation("cardClubs3_1");
+card2.scale = 0.2;
+var card3 = createSprite(200, 200);
+card3.setAnimation("cardClubsA_1");
+card3.scale = 0.3;
+var card4 = createSprite(200, 200);
+card4.setAnimation("cardClubs10_1");
+card4.scale = 0.3;
+card4.height = 0.2;
+var card5 = createSprite(200, 200);
+card5.setAnimation("cardClubs10_1");
+card5.scale = 0.3;
+card5.height = 0.2;
 }

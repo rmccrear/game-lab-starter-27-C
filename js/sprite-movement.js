03f1f1bd3a239d6f15acee8b522fd6e4db6ba951
honeybees00 <Cortizmason@outlook.com>
2025-08-07 20:29:21 -0400

    changed variables and added sprite movement

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -1,6 +1,48 @@
 
-function doSpriteMovement() {
-    // Example Code follows.
-    // Please delete and replace with your own code.
-   
-}
+  function doSpriteMovement(){
+  // FALLING
+  player.y = player.y + 3;
+  card2.y = card2.y + 10;
+  card.x = card.x + 8;
+  card3.x = card3.x + 3;
+  card4.y = card4.y + 5;
+  card5.x = card5.x - 5;
+  
+  
+  // LOOPING
+  if(player.x > 420){player.x = -20;}
+  if(player.x < -20){player.x = 420;}
+  if(player.y > 420){player.y = -20;}
+  if(player.y < -20){player.y = 420;}
+  
+  // rock1 location
+  if(card.x > 410){card.x = -10;}
+  if(card.x < -10){card.x = 410;}
+  if(card.y > 410){card.y = -10;}
+  if(card.y < -10){card.y = 410;}
+  
+  // rock2 location
+  if(card2.y > 410){card2.y = -10;}
+  if(card2.y < -10){card2.y = 410;}
+  if(card2.x < -10){card2.x = 410;}
+  if(card2.x > 410){card2.x = -10;}
+  
+  // rock3 location
+  if(card3.x > 410){card3.x = -10;}
+  if(card3.x < -10){card3.x = 410;}
+  if(card3.y > 410){card3.y = -10;}
+  if(card3.y < -10){card3.y = 410;}
+  
+  // rock4 location
+  if(card4.y > 410){card4.y = -10;}
+  if(card4.y < -10){card4.y = 410;}
+  if(card4.x < -10){card4.x = 410;}
+  if(card4.x > 410){card4.x = -10;}
+  
+  // rock5 location
+  if(card5.y > 410){card5.y = -10;}
+  if(card5.y < -10){card5.y = 410;}
+  if(card5.x < -10){card5.x = 410;}
+  if(card5.x > 410){card5.x = -10;}
+  }
+  

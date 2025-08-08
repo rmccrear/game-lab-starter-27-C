915788cd62567656749c6a78bea81e7966b9feea
Lashon81 <lashon_riley81@yahoo.com>
2025-08-07 21:35:37 -0400

    animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,22 +1,8 @@
 
 function setupAnimations() {
-  var player = createSprite(200, 100);
-  player.setAnimation("kid_34_excited_1");
-  player.scale = 0.2;
-  var coins = createSprite(364, 19);
-  coins.setAnimation("coin");
-  var card = createSprite(6, 140);
-  card.setAnimation("cardClubsQ_1");
-  card.scale = 0.3;
-  var card2 = createSprite(297, 6);
-  card2.setAnimation("cardClubs3_1");
-  card2.scale = 0.2;
-  var card3 = createSprite(200, 200);
-  card3.setAnimation("cardClubsA_1");
-  card3.scale = 0.3;
-  var card4 = createSprite(200, 200);
-  card4.setAnimation("cardClubs10_1");
-  var card5 = createSprite(200, 200);
-  card5.setAnimation("cardClubs10_1");
-
+  card = loadAnimation("assets/cardClubsQ_1.png");
+  card2 = loadAnimation("assets/cardClubs3_1.png");
+  card3 = loadAnimation("assets/cardClubs10_1.png");
+  card4 = loadAnimation("assets/cardClubsA_1.png");
+  player = loadAnimation("assets/kid_34_excited.png");
 }

9afdcf54622844bebc1cf7d095fa710a1766f362
Lashon81 <lashon_riley81@yahoo.com>
2025-08-07 22:06:57 -0400

    made changes

--- js/user-controls.js
+++ js/user-controls.js
@@ -1,18 +1,18 @@
 
 function respondToUser() {
     if (keyDown("UP_ARROW")) {
-        player.velocityY =  0.5;
+        player.velocityY = 0.5;
 
     }
     // change the y velocity when the user clicks "down"
     if (keyDown("DOWN_ARROW")) {
-        player.velocityY =  + 0.5;
+        player.velocityY = + 0.5;
 
     }
 
     // decrease the x velocity when user clicks "left"
     if (keyDown("LEFT_ARROW")) {
-        player.velocityX = p- 0.5;
+        player.velocityX = - 0.5;
     }
 
     // increase the x velocity when the user clicks "right"
